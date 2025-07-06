a36ac7df698816dd5be5c23e5c196ba02f6d4ed2
komalanami <kumarikomal1511@gmail.com>
2025-07-06 13:21:47 +0530

    btn disable for more then one click

--- src/components/GameBoard.js
+++ src/components/GameBoard.js
@@ -29,8 +29,14 @@ export default function GameBoard({ onSelectSquare, turns }) {
     return <ol id="game-board">
         {gameBoard.map((row, rowIndex) => <li key={rowIndex}>
             <ol>
-                {row.map((playerSymbol, colIndex) => <li key={colIndex}>
-                    <button onClick={() => onSelectSquare(rowIndex, colIndex)}>{playerSymbol}</button></li>)}
+                {row.map((playerSymbol, colIndex) =>
+                    <li key={colIndex}>
+                        <button onClick={() => onSelectSquare(rowIndex, colIndex)}
+                            disabled={playerSymbol !== null} >
+                            {playerSymbol}
+                        </button>
+                    </li>
+                )}
             </ol>
         </li>)}
     </ol>
